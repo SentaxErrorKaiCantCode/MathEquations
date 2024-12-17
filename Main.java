cc0d2795b273e7c88ea862a54097bc23c6888ce7
SentaxErrorKaiCantCode <kaimolchon@student.scit.org>
2024-12-17 18:21:06 +0000

    commi

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main extends Slope1  { 
+        public static void main(String[] args) {
+            slope1();
+        }
+
+public class Main extends Slope2  {
+        public static void main(String[] args) {
+            slope2();
+        }
+    }
+}
---
+++ Slope1.java
@@ -0,0 +1,12 @@
+
+public class Slope1 {
+    static void slope1() {
+    // pulg in your variables in the corosponding spot 
+    int x1 = 4;
+    int y1 = 2;
+    int x2 = 2;
+    int y2 = 1;
+    System.out.println(y2 - y1/ x2 - x1) ;
+    
+}
+}
